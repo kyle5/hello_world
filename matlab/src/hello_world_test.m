4fb6294a91de30c4e05014af48498bf8a8a035f2
kyle wilshusen <kyle@kyle-All-Series.(none)>
2014-03-15 21:35:47 -0400

    hello world testing file adjusted 4

--- matlab/src/hello_world_test.m
+++ matlab/src/hello_world_test.m
@@ -2,4 +2,7 @@ function hello_world_test()
 	disp('Hello world from MATLAB');
 	fid = fopen( '/home/kyle/apparently_autobuild_works.txt', 'w' );
 	fclose( fid );
+
+	addpath('../../cpp/build/');
+	hello_world_mex();
 end
